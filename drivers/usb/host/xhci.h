256a38fe5a12e8fcf038f2658d5e5164811864ec
minda.chen <minda.chen@starfivetech.com>
2022-10-18 09:57:39 +0800

    usb:xhci:To improve performance,usb using lowmem for bulk xfer.
    Generate an usb low memory pool for usb 3.0 host
    read/write transfer, default size is 8M.
    
    Signed-off-by: minda.chen <minda.chen@starfivetech.com>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1479,6 +1479,13 @@ struct xhci_hub {
 	u8			min_rev;
 };
 
+struct xhci_lowmem_pool {
+	struct gen_pool *pool;
+	u64		cached_base;
+	dma_addr_t	dma_addr;
+	unsigned int	size;
+};
+
 /* There is one xhci_hcd structure per controller */
 struct xhci_hcd {
 	struct usb_hcd *main_hcd;
@@ -1627,6 +1634,8 @@ struct xhci_hcd {
 #define XHCI_CDNS_SCTX_QUIRK	BIT_ULL(48)
 #define XHCI_ETRON_HOST	BIT_ULL(49)
 
+#define XHCI_LOCAL_BUFFER	BIT_ULL(63)
+
 	unsigned int		num_active_eps;
 	unsigned int		limit_active_eps;
 	struct xhci_port	*hw_ports;
@@ -1653,6 +1662,8 @@ struct xhci_hcd {
 	struct list_head	regset_list;
 
 	void			*dbc;
+	struct xhci_lowmem_pool lowmem_pool;
+
 	/* platform-specific data -- must come last */
 	unsigned long		priv[] __aligned(sizeof(s64));
 };
